c8ba57408a200c47b5f571ede447e1322046bb0a
dmytroafr <afrosin.dmytro@gmail.com>
2023-06-18 09:54:59 +0300

    Start of project

---
+++ src/main/java/com/echem/ecshop/EcshopApplication.java
@@ -0,0 +1,13 @@
+package com.echem.ecshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/echem/ecshop/EcshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echem.ecshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
