8a9b5c5109a6b2b1d49553ada0ba5f3761801b4c
dmytroafr <afrosin.dmytro@gmail.com>
2023-06-25 11:25:41 +0300

    add sql - validate

--- src/main/java/com/echem/ecshop/EcshopApplication.java
+++ src/main/java/com/echem/ecshop/EcshopApplication.java
@@ -2,9 +2,19 @@ package com.echem.ecshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 @SpringBootApplication
 public class EcshopApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(EcshopApplication.class, args);
+
+//		SpringApplication.run(EcshopApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(EcshopApplication.class, args);
+		PasswordEncoder encoder = context.getBean(PasswordEncoder.class);
+		System.out.println(encoder.encode("pass"));
+
+
+
 	}
 }
--- src/main/resources/db/migration/V2_add_admin.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO users (id, archive, email, name, password, role, bucket_id)
-VALUES (1, false, 'office@e-chem.com.ua', admin, 'password', 'ADMIN', null);
-
-ALTER SEQUENCE user_seq RESTART WITH 2;
