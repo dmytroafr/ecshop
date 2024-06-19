d9c9b683caac7a15ccc809250a5c37a042c1b5dc
dmytroafr <afrosin.dmytro@gmail.com>
2024-06-19 18:17:33 +0300

    orders

--- src/main/java/com/echem/ecshop/dto/OrderDTO.java
+++ src/main/java/com/echem/ecshop/dto/OrderDTO.java
@@ -3,11 +3,11 @@ package com.echem.ecshop.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderDTO {
-
     private String delivery;
     private String payment;
 }
--- src/main/java/com/echem/ecshop/service/email/EmailService.java
+++ src/main/java/com/echem/ecshop/service/email/EmailService.java
@@ -1,21 +1,28 @@
 package com.echem.ecshop.service.email;
 
-import lombok.AllArgsConstructor;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
 
 @Service
-@AllArgsConstructor
 public class EmailService implements EmailSender{
 
     private static final Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
     private final JavaMailSender mailSender;
+
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    @Value("${spring.mail.username}")
+    private String mailFrom;
 
     @Override
     @Async
@@ -26,7 +33,7 @@ public class EmailService implements EmailSender{
             helper.setText(email);
             helper.setTo(to);
             helper.setSubject(subject);
-            helper.setFrom("java@e-chem.com.ua");
+            helper.setFrom(mailFrom);
             mailSender.send(mimeMessage);
 
         } catch (MessagingException e) {
