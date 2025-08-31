ea570876964662293200239eef43d883dc806d2d
dmytroafr <afrosin.dmytro@gmail.com>
2025-08-31 13:28:27 +0300

    changes

--- src/main/java/com/echem/ecshop/config/HttpConfiguration.java
+++ src/main/java/com/echem/ecshop/config/HttpConfiguration.java
@@ -1,26 +1,26 @@
-package com.echem.ecshop.config;
+// package com.echem.ecshop.config;
 
-import org.apache.catalina.connector.Connector;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.apache.catalina.connector.Connector;
+// import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+// import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class HttpConfiguration {
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-        tomcat.addAdditionalTomcatConnectors(httpConnector());
-        return tomcat;
-    }
+// @Configuration
+// public class HttpConfiguration {
+//     @Bean
+//     public ServletWebServerFactory servletContainer() {
+//         TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+//         tomcat.addAdditionalTomcatConnectors(httpConnector());
+//         return tomcat;
+//     }
 
-    private Connector httpConnector() {
-        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-        connector.setScheme("http");
-        connector.setPort(80);
-        connector.setSecure(false);
-        connector.setRedirectPort(443);
-        return connector;
-    }
-}
+//     private Connector httpConnector() {
+//         Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+//         connector.setScheme("http");
+//         connector.setPort(80);
+//         connector.setSecure(false);
+//         connector.setRedirectPort(443);
+//         return connector;
+//     }
+// }
